c7fed0c21144ea93260cc862fd55d1f9da3de743
wdlth <wdlth@wdlth.com>
2021-05-07 00:42:48 +0800

    Update checksum checking.

--- build_tools/download_and_unzip.sh
+++ build_tools/download_and_unzip.sh
@@ -6,7 +6,7 @@ curl -JOL "$GRADLE_URL"
 GRADLE_CHECKSUM_URL="https://services.gradle.org/distributions/gradle-6.8.2-bin.zip.sha256"
 curl -JOL "$GRADLE_CHECKSUM_URL"
 
-sha256sum -c gradle-6.8.2-bin.zip.sha256
+diff -Z <(sha256sum gradle-6.8.2-bin.zip | awk '{print $1}') <(cat gradle-6.8.2-bin.zip.sha256)
 mv ./gradle-6.8.2-bin.zip /opt
 
 MAVEN_URL="https://mirrors.ocf.berkeley.edu/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip"
@@ -15,12 +15,12 @@ curl -JOL "$MAVEN_URL"
 MAVEN_CHECKSUM_URL="https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip.sha512"
 curl -JOL "$MAVEN_CHECKSUM_URL"
 
-sha512sum -c apache-maven-3.6.3-bin.zip.sha512
+diff -Z <(sha512sum apache-maven-3.6.3-bin.zip | awk '{print $1}') <(cat apache-maven-3.6.3-bin.zip.sha512)
 mv ./apache-maven-3.6.3-bin.zip /opt
 
 cd /opt || exit
-gunzip gradle-6.8.2-bin.zip
-gunzip apache-maven-3.6.3-bin.zip
+unzip gradle-6.8.2-bin.zip
+unzip apache-maven-3.6.3-bin.zip
 
 rm -f gradle-6.8.2-bin.zip apache-maven-3.6.3-bin.zip
 
