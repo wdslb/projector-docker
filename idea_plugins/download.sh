fb387822a6d6c2ba28675dbe1ecfad8ae782b491
wdlth <wdlth@wdlth.com>
2021-09-13 00:46:06 +0800

    Update: 2021-09-13
    1. Fix Travis CI config bug about Ubuntu 20.04.

--- idea_plugins/download.sh
+++ idea_plugins/download.sh
@@ -4,7 +4,7 @@ PRODUCT_CODE="IC"
 BUILD_NUMBER="203.7148.57"
 
 while read -r line; do
-  eval "$(printf "curl -JLO \"https://plugins.jetbrains.com/pluginManager?action=download&id=%s&build=%s-%s\"\n" "$line" "$PRODUCT_CODE" "$BUILD_NUMBER")"
+  eval "$(printf "curl -sJLO \"https://plugins.jetbrains.com/pluginManager?action=download&id=%s&build=%s-%s\"\n" "$line" "$PRODUCT_CODE" "$BUILD_NUMBER")"
 done < list.txt
 
 IDEA_SPRING_TOOLS_URL=$(curl -s "https://api.github.com/repos/wdslb/idea-spring-tools/releases/latest" | jq -r '.assets[0].browser_download_url')
